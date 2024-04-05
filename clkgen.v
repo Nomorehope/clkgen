3e90ad40b3e91beb3417896eccb338f190f9f99e
Nomorehope <54616900+Nomorehope@users.noreply.github.com>
2024-04-05 08:07:57 +0300

    Чистейший clkgen

--- clkgen.v
+++ clkgen.v
@@ -1,43 +1,24 @@
-module clkgen(input clk,  button, output reg exit, exitbutton, ledbutton); //ledgen
+module clkgen(input clk, output reg ti); //ledgen
 wire tempwire;
 reg [13:0]cnt;
 
 genpll genpll(.inclk0(clk), .c0(tempwire));
-always @ (posedge clk) begin
-if (button == 1'b1) begin
-exitbutton <= 1;
-ledbutton <= 0;
-end
-else begin
-exitbutton <= 1'b0;
-ledbutton <= 1'b1;
-end
-end
 
 always @ (posedge tempwire) begin
 if (cnt >= 14'd10240)
-//if (cnt >= 24'd1000000)
 begin
 cnt <= 1'b0;
-exit <= 1'b0;
-
-//ledgen <= 0; // new
+ti <= 1'b0;
 
 end
 
-//else if (cnt >= 24'd100000) // new
-//begin
-//ledgen <= 1;
-//cnt <= cnt + 1;
-//end
-
 else if(cnt < 2'd2)
 begin
-exit <= 1'b1;
+ti <= 1'b1;
 cnt = cnt + 1'b1;
 end else begin
 cnt = cnt + 1'b1;
-exit = 0;
+ti = 0;
 end
 end
 endmodule
